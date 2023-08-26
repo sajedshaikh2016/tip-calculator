6f1ae70feabacaad854c34fe730a903385be01e8
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-08-26 22:02:35 +0530

    Add Result View

---
+++ tip-calculator/extension/UIView_Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView_Extension.swift
+//  tip-calculator
+//
+//  Created by Sajed Shaikh on 26/08/23.
+//
+
+import UIKit
+
+extension UIView {
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+}
