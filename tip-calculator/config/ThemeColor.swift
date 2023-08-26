8a474599578505b811423aacf9467eb7ce365c24
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-08-26 17:04:18 +0530

    Add Theme Constants

---
+++ tip-calculator/config/ThemeColor.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColor.swift
+//  tip-calculator
+//
+//  Created by Sajed Shaikh on 26/08/23.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let bg = UIColor("F5F3F4")
+    static let primary = UIColor("1CC9BE")
+    static let secondary = UIColor.systemOrange
+    static let text = UIColor("000000")
+    static let separator = UIColor("CCCCCC")
+}
---
+++ tip-calculator/config/ThemeFont.swift
@@ -0,0 +1,22 @@
+//
+//  ThemeFont.swift
+//  tip-calculator
+//
+//  Created by Sajed Shaikh on 26/08/23.
+//
+
+import UIKit
+
+struct ThemeFont {
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
