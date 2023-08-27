403efc216a2abe34294bb5e0e196aa7bd11a0370
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-08-27 12:18:57 +0530

    Add Amount View

--- tip-calculator/view/ResultView.swift
+++ tip-calculator/view/ResultView.swift
@@ -50,9 +50,13 @@ class ResultView: UIView {
     
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            AmountView(),
+            AmountView(
+                title: "Totle bill",
+                textAlignment: .left),
             UIView(),
-            AmountView()
+            AmountView(
+                title: "Totle tip",
+                textAlignment: .right)
         ])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
@@ -92,20 +96,5 @@ class ResultView: UIView {
         let view = UIView()
         view.heightAnchor.constraint(equalToConstant: height).isActive = true
         return view
-    }
-}
-
-class AmountView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        layout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func layout() {
-        backgroundColor = .red
     }
 }
