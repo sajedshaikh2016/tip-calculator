2a4cdfed97c2df2670da2fed122325cc9008bafb
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-08-27 17:48:34 +0530

    Add View Model

---
+++ tip-calculator/model/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Sajed Shaikh on 27/08/23.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
---
+++ tip-calculator/view model/CalculatorVM.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorVM.swift
+//  tip-calculator
+//
+//  Created by Sajed Shaikh on 27/08/23.
+//
+
+import Foundation
+import Combine
+
+class CalculatorVM {
+    
+    struct Input {
+        let billPublisher: AnyPublisher<Double, Never>
+        let tipPublisher: AnyPublisher<Tip, Never>
+        let splitPublisher: AnyPublisher<Int, Never>
+    }
+    
+    struct Output {
+        let updateViewPublisher: AnyPublisher<Result, Never>
+    }
+    
+    func transform(input: Input) -> Output {
+        let result = Result(
+            amountPerPerson: 500,
+            totalBill: 1000,
+            totalTip: 50.5)
+        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+    }
+}
