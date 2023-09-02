305bf529fd83ef849086caaafaacd36fe3f151b9
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-09-02 12:20:03 +0530

    Display Result In Result View

--- tip-calculator/view/ResultView.swift
+++ tip-calculator/view/ResultView.swift
@@ -18,7 +18,7 @@ class ResultView: UIView {
         let label = UILabel()
         label.textAlignment = .center
         let text = NSMutableAttributedString(
-            string: "0$",
+            string: "$0",
             attributes: [
                 .font: ThemeFont.bold(ofSize: 48)
             ])
@@ -48,15 +48,25 @@ class ResultView: UIView {
         return stackView
     }()
     
+    private let totalBillView: AmountView = {
+        let view = AmountView(
+            title: "Totle bill",
+            textAlignment: .left)
+        return view
+    }()
+    
+    private let totalTipView: AmountView = {
+        let view = AmountView(
+            title: "Totle tip",
+            textAlignment: .right)
+        return view
+    }()
+    
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            AmountView(
-                title: "Totle bill",
-                textAlignment: .left),
+            totalBillView,
             UIView(),
-            AmountView(
-                title: "Totle tip",
-                textAlignment: .right)
+            totalTipView
         ])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
@@ -70,6 +80,19 @@ class ResultView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(result: Result) {
+        let text = NSMutableAttributedString(
+            string: String(result.amountPerPerson),
+            attributes: [.font: ThemeFont.bold(ofSize: 48)])
+        text.addAttributes([
+            .font: ThemeFont.bold(ofSize: 24)
+        ], range: NSMakeRange(0, 1))
+        amountPerPersonalLabel.attributedText = text
+        
+        totalBillView.configure(text: String(result.totalBill))
+        totalTipView.configure(text: String(result.totalTip))
     }
     
     private func layout() {
