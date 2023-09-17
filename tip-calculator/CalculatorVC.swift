80e8a24b403d83feb3f02c3a840c6d935874e680
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-09-17 12:26:42 +0530

    Implement Calculator Reset

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -48,7 +48,7 @@ class CalculatorVC: UIViewController {
     private lazy var logoViewTapPublisher: AnyPublisher<Void, Never> = {
         let tapGesture = UITapGestureRecognizer(target: self, action: nil)
         tapGesture.numberOfTapsRequired = 2
-        view.addGestureRecognizer(tapGesture)
+        logoView.addGestureRecognizer(tapGesture)
         return tapGesture.tapPublisher.flatMap { _ in
             Just(())
         }.eraseToAnyPublisher()
@@ -74,18 +74,30 @@ class CalculatorVC: UIViewController {
             resultView.configure(result: result)
         }.store(in: &cancellables)
         
-        output.resultCalculatorPublisher.sink { _ in
-            print("Hey, reset the form please!")
+        output.resetCalculatorPublisher.sink { [unowned self] _ in
+            billInputView.reset()
+            tipInputView.reset()
+            splitInputView.reset()
+            
+            UIView.animate(
+                withDuration: 0.1,
+                delay: 0,
+                usingSpringWithDamping: 5.0,
+                initialSpringVelocity: 0.5,
+                options: .curveEaseInOut) {
+                    self.logoView.transform = .init(scaleX: 1.5, y: 1.5)
+                } completion: { _ in
+                    UIView.animate(withDuration: 0.1) {
+                        self.logoView.transform = .identity
+                    }
+                }
+            
         }.store(in: &cancellables)
     }
     
     private func observe() {
         viewTapPublisher.sink { [unowned self] value in
             view.endEditing(true)
-        }.store(in: &cancellables)
-        
-        logoViewTapPublisher.sink { _ in
-            print("Logo view is tapped")
         }.store(in: &cancellables)
     }
     
