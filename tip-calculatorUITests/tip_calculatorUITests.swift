4ad74dd46537a052cca97ea6a79f328f44730848
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-14 20:28:00 +0530

    Update tip_calculatorUITests.swift

--- tip-calculatorUITests/tip_calculatorUITests.swift
+++ tip-calculatorUITests/tip_calculatorUITests.swift
@@ -31,6 +31,7 @@ final class tip_calculatorUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
+        
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
